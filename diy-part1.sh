a4375b983240245c3a7dbe0420831a5144f30f95
cricfer <117727157+cricfer@users.noreply.github.com>
2024-02-08 16:06:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 #echo 'src-git oaf https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+echo 'src-git smpackage https://github.com/kenzok8/small-package' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
